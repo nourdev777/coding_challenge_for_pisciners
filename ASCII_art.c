bb7e58475e2053b86a62552232b753eb754d97bc
nourdev777 <nourdev777@gmail.com>
2025-09-04 21:17:21 +0400

    ascii template

---
+++ ASCII_art.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+    int i = 0;
+    while (i < 3) {
+        printf("   ***   \n");
+        printf("  *   *  \n");
+        printf(" *Camel* \n");
+        sleep(1);
+        i++;
+    }
+    return 0;
+}
